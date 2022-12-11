8f6b61850a80f02d18154231f2137844f86fe237
sujith-hub <gurrapusujith@gmail.com>
2022-12-11 20:37:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+//console.warn("Welcome to bibilet");
+
+    // let elemClass=document.getElementsByClassName('container');
+    // console.log(elemClass);
+
+    // elemClass[0].style.background = "yellow";
+    // elemClass[0].classList.add();
+
+    function clicked()
+    {
+        console.log('Welcome to bibilet');
+    }
+    function validate()
+    {
+        var username=document.getElementById().Value;
+        var password=document.getElementById().Value;
+        
+        if(username=="admin"&&password=="user")
+        {
+            alert("Login successfull");
+            return false;
+
+        }
+        else
+        {
+            alert("Login Failed");
+            return false;
+        }
+    }
+    // let prevHTML=document.querySelectorAll('.container')[1].innerHTML;
+    // firstcontainer.addEventListener('mouseup',function()
+    // {
+    //     document.querySelectorAll('.container')[1].innerHTML=prevHTML;
+    //     console.log("mouseup clicked")
+    // })
+    // firstcontainer.addEventListener('mousedown',function()
+    // {
+    //     document.querySelectorAll('.container')[1].innerHTML="<b>we have Clicked</b>"
+    //     console.log("mouse down clicked")
+    // })
+
+    // logk = () =>{
+    //     console.log("I am ur log");
+    // }
+    // setTimeout(logk, 2000);
+
+    // obj={name: "sujith",length: 1, a: {this: 'tha"t'}}
+    // jso=JSON.stringify(obj);
+    // console.log(typeof jso)
+    // console.log(jso)
+    // parsed=JSON.parse('{"name":"sujith","length":1,"a":{"this":"tha\\"t"}}')
+    // console.log(parsed);
